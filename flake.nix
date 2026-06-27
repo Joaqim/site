fd92583d8cbc2847d5a9ebb64ab9474e4399a8cb
Joaqim Planstedt <mail@joaqim.xyz>
2026-06-27 18:59:44 +0200

    feat: add logo.png and nix utility at 'nix run .#icons' to generate icons in `./static/`
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>

--- flake.nix
+++ flake.nix
@@ -63,6 +63,31 @@
                       --watch static \
                       ${pkgs.lib.getExe final.speedy} generate
                 '';
+                generate-static-icons =
+                  let
+                    convert = "${pkgs.lib.getExe pkgs.imagemagick}";
+                  in
+                  pkgs.writeShellScriptBin "generate-static-icons" ''
+                    # [name]=size
+                    declare -A sizes=(
+                      [android-chrome-192x192]=192
+                      [android-chrome-384x384]=384
+                      [apple-touch-icon]=180
+                      [favicon-16x16]=16
+                      [favicon-32x32]=32
+                      [mstile-150x150]=150
+                    )
+
+                    # various .png
+                    for name in "''${!sizes[@]}"; do
+                      ${convert} logo.png -resize "''${sizes[$name]}x''${sizes[$name]}" "./static/''${name}.png"
+                    done
+
+                    # favicon.ico
+                    ${convert} logo.png -resize 16x16 \
+                            \( logo.png -resize 32x32 \) \
+                            ./static/favicon.ico
+                  '';
               }
             )
           ];
@@ -107,6 +132,7 @@
           {
             build = mkApp build;
             serve = mkApp serve;
+            icons = mkApp generate-static-icons;
             default = mkApp serve;
           };
 
