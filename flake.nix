2f9e60234506831db2aa30473821bfd7a32a61e7
Joaqim Planstedt <mail@joaqim.xyz>
2026-06-28 14:35:30 +0200

    refactor: use flake-utils with treefmt-nix module for typos, dprint, shellcheck and shfmt
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>

--- flake.nix
+++ flake.nix
@@ -2,144 +2,177 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://speedy.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "speedy.cachix.org-1:GtGlHdmJ8q1MzD40sbY45hVv3Xws0RhDp/4bac4tGKQ="
     ];
   };
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    # Provides some nice helpers for multiple system compatibility
-    flake-utils.url = "github:numtide/flake-utils";
-
+    # Extended utilities for working with nix flake modules
+    flake-parts.url = "github:hercules-ci/flake-parts";
     # Generate Javascript, HTML and CSS for personal blog
     speedy = {
       url = "gitlab:joaqim/speedy/master";
       # No need for extended compatibility
       inputs.flake-compat.follows = "";
     };
+    # https://nixos.asia/en/treefmt
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-root.url = "github:srid/flake-root";
   };
 
   outputs =
     inputs@{
       nixpkgs,
-      flake-utils,
+      flake-parts,
+      treefmt-nix,
+      flake-root,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (
-              final: prev:
-              let
-                inherit (inputs.speedy.packages.${system}) speedy;
-                build = pkgs.writeShellScriptBin "build" ''
-                  [ -d ./public ] && rm -r ./public
-                  speedy generate
-                '';
-              in
-              {
-                inherit build;
-                speedy = speedy.overrideAttrs (_: {
-                  postPatch = ''
-                    rm -r ./templates
-                    cp -RL --no-preserve=mode ${./templates} ./templates
-                  '';
-                  meta.mainProgram = "speedy";
-                });
-                serve = pkgs.writeShellScriptBin "serve" ''
-                  ${pkgs.lib.getExe build}
-                  speedy run &
-                  trap 'jobs -p | xargs kill' EXIT
-                  watchexec \
-                      --watch posts \
-                      --watch static \
-                      ${pkgs.lib.getExe final.speedy} generate
-                '';
-                generate-icons = pkgs.writeShellScriptBin "generate-icons" (
-                  builtins.readFile ./scripts/generate-icons.sh
-                );
-                # Adapted from: https://stackoverflow.com/a/76264351
-                get-first-github-commit-date = pkgs.writeShellApplication {
-                  name = "get-first-github-commit-date";
-                  runtimeInputs = with pkgs; [
-                    curlMinimal
-                    gawk
-                  ];
-                  text = builtins.readFile ./scripts/get-first-github-commit-date.sh;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        treefmt-nix.flakeModule
+        flake-root.flakeModule
+      ];
+
+      perSystem =
+        {
+          system,
+          lib,
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          # Auto formatters. This also adds a flake check to ensure that the
+          # source tree was auto formatted.
+          treefmt.config = {
+            inherit (config.flake-root) projectRootFile;
+            package = pkgs.treefmt;
+            programs = {
+              dprint = {
+                enable = true;
+                includes = [ "**/*.md" ];
+                settings = {
+                  plugins = pkgs.dprint-plugins.getPluginList (
+                    plugins: with plugins; [
+                      dprint-plugin-markdown
+                    ]
+                  );
                 };
-                create-new-post = pkgs.writeShellScriptBin "create-new-post" (
-                  builtins.readFile ./scripts/create-new-post.sh
-                );
-                create-project-from-github = pkgs.writeShellScriptBin "create-project-from-github" (
-                  builtins.readFile ./scripts/create-project-from-github.sh
-                );
-              }
-            )
-          ];
-        };
-        highlightjs = pkgs.callPackage ./highlightjs.nix { };
-      in
-      # "unpack" the pkgs attrset into the parent namespace
-      with pkgs;
-      {
-        devShells = {
-          default = mkShell {
-            buildInputs = [
-              bashInteractive
-              coreutils
-              watchexec
-
-              dprint
-              typos
-
-              speedy
-              generate-icons
-              create-new-post
-              get-first-github-commit-date
-              create-project-from-github
-            ];
-            ENVIRONMENT = "dev";
-            AUTHOR_FULLNAME = "Joaqim Planstedt";
-            URL_BASE = "blog.joaqim.com";
-            PROJECTS_SORT_ORDER = "site,speedy,plan,dotfiles,jqpkgs,balanced-dice-avr";
-
-            shellHook = ''
-              [ -d ./static/js/vendor/highlight ] || cp -RL --no-preserve=mode "${highlightjs}" ./static/js/vendor/highlight
-            '';
-          };
-          ci = mkShell {
-            buildInputs = [
-              bashNonInteractive
-            ];
-          };
-        };
-
-        apps =
-          let
-            mkApp = pkg: {
-              type = "app";
-              program = lib.getExe pkg;
+              };
+              typos = {
+                enable = true;
+                configFile = "typos.toml";
+              };
+              shfmt.enable = true;
+              shellcheck.enable = true;
             };
-          in
-          {
-            build = mkApp build;
-            serve = mkApp serve;
-            icons = mkApp generate-icons;
-            default = mkApp serve;
           };
 
-        packages = rec {
-          site = callPackage ./site.nix { inherit speedy highlightjs; };
-          default = site;
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (
+                final: prev:
+                let
+                  inherit (inputs.speedy.packages.${system}) speedy;
+                  build = pkgs.writeShellScriptBin "build" ''
+                    git clean -xdf ./public
+                    speedy generate
+                  '';
+                in
+                {
+                  inherit build;
+                  speedy = speedy.overrideAttrs (_: {
+                    postPatch = ''
+                      rm -r ./templates
+                      cp -RL --no-preserve=mode ${./templates} ./templates
+                    '';
+                    meta.mainProgram = "speedy";
+                  });
+                  serve = pkgs.writeShellScriptBin "serve" ''
+                    ${lib.getExe build}
+                    speedy run &
+                    trap 'jobs -p | xargs kill' EXIT
+                    watchexec \
+                        --watch posts \
+                        --watch static \
+                        ${lib.getExe final.speedy} generate
+                  '';
+                  generate-icons = pkgs.writeShellScriptBin "generate-icons" (
+                    builtins.readFile ./scripts/generate-icons.sh
+                  );
+                  # Adapted from: https://stackoverflow.com/a/76264351
+                  get-first-github-commit-date = pkgs.writeShellApplication {
+                    name = "get-first-github-commit-date";
+                    runtimeInputs = with pkgs; [
+                      curlMinimal
+                      gawk
+                    ];
+                    text = builtins.readFile ./scripts/get-first-github-commit-date.sh;
+                  };
+                  create-new-post = pkgs.writeShellScriptBin "create-new-post" (
+                    builtins.readFile ./scripts/create-new-post.sh
+                  );
+                  create-project-from-github = pkgs.writeShellScriptBin "create-project-from-github" (
+                    builtins.readFile ./scripts/create-project-from-github.sh
+                  );
+                  highlightjs = pkgs.callPackage ./highlightjs.nix { };
+                }
+              )
+            ];
+          };
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                bashInteractive
+                coreutils
+
+                speedy
+                generate-icons
+                create-new-post
+                get-first-github-commit-date
+                create-project-from-github
+              ];
+              ENVIRONMENT = "dev";
+              AUTHOR_FULLNAME = "Joaqim Planstedt";
+              URL_BASE = "blog.joaqim.com";
+              PROJECTS_SORT_ORDER = "site,speedy,plan,dotfiles,jqpkgs,balanced-dice-avr";
+
+              shellHook = ''
+                [ -d ./static/js/vendor/highlight ] || cp -RL --no-preserve=mode "${pkgs.highlightjs}" ./static/js/vendor/highlight
+              '';
+            };
+            ci = pkgs.mkShell {
+              buildInputs = [
+                pkgs.bashNonInteractive
+              ];
+            };
+          };
+
+          apps =
+            let
+              mkApp = pkg: {
+                type = "app";
+                program = lib.getExe pkg;
+              };
+            in
+            with pkgs;
+            {
+              build = mkApp build;
+              serve = mkApp serve;
+              icons = mkApp generate-icons;
+              default = mkApp serve;
+            };
+
+          packages = rec {
+            site = pkgs.callPackage ./site.nix { inherit (pkgs) speedy highlightjs; };
+            default = site;
+          };
         };
-      }
-    );
+    };
 }
